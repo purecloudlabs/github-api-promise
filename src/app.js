b82123b594b6b1d66f4ec86614f005430fceff18
Tim Smith <tim.j.smith@genesys.com>
2017-10-06 15:51:13 -0600

    refactor app.js

--- src/app.js
+++ src/app.js
@@ -2,8 +2,13 @@ var githubApi = {};
 
 // Submodules
 githubApi.config = require('./config');
-githubApi.repos = require('./repositories/repositories');
-githubApi.activity = require('./activity/activity');
+githubApi.repositories = {
+	releases: require('./repositories/releases'),
+};
+githubApi.repos = githubApi.repositories; // for backwards compatibility
+githubApi.activity = {
+	events: require('./activity/events')
+};
 
 // Set to window object if there is a window
 if(typeof window !== 'undefined') {
