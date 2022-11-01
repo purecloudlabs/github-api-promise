1c1c1c12bd9162228e00ed9adddb97f9769dfe98
Ebenezer Osei <Ebenezer.Osei@genesys.com>
2022-11-01 18:00:58 -0400

    Corrected rollup configuration

--- build/request-helpers.d.ts
+++ build/request-helpers.d.ts
@@ -3,7 +3,7 @@ declare class Request {
     private request;
     constructor();
     private logRequestSucess;
-    assembleQueryParams(params: any, paramNames: string[]): string;
+    assembleQueryParams(params: any, paramNames: string[]): any;
     standardRequest(url: string, method?: string, body?: any): Promise<unknown>;
     extendedRequest(url: string, method?: string, body?: any): Promise<unknown>;
 }
